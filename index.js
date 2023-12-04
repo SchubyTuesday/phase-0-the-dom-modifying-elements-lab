31a77d4c20c48bef1309cf140ff1915661ecf765
Charlie Schubach <charlie.schubach@gmail.com>
2023-12-04 13:59:50 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+const mainElement = document.getElementById('main');
+mainElement.remove();
+
+// Create a new <h1> element
+const newHeader = document.createElement('h1');
+
+// Set the id of the newHeader element
+newHeader.id = 'victory';
+
+// Set the text inside newHeader
+newHeader.textContent = 'Charlie is the champion';
